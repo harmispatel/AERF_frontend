59b5d92586acdf11bf37447c358fa544d9ef130a
unknown <harmistest@gmail.com>
2023-06-26 15:36:33 +0530

    summary page

--- src/pages/ProjectReport/Village.jsx
+++ src/pages/ProjectReport/Village.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import "../index.css";
+import { FaAngleDown } from "react-icons/fa6";
 
 const Village = () => {
   return (
-    <div>Village</div>
+    <>
+      <div className="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
+        <div className='village_main'>
+          <div className='fillter_part'>
+            <div className='container'>
+              <div className='row align-items-center'>
+                <div className='col-md-2'>
+                  <div className='fillter_box'>
+                    <h3>Fillter</h3>
+                  </div>
+                </div>
+                <div className='col-md-2'>
+                  <div className='fillter_box'>
+                    <div className='form-group'>
+                      <select className='form-select form-control'>
+                        <option>Village</option>
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                      </select>
+                      <span><FaAngleDown className='icon' /></span>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-2'>
+                  <div className='fillter_box'>
+                    <div className='form-group'>
+                      <select className='form-select form-control'>
+                        <option>Wadi</option>
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                      </select>
+                      <span><FaAngleDown className='icon' /></span>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-2'>
+                  <div className='fillter_box'>
+                    <div className='form-group'>
+                      <select className='form-select form-control'>
+                        <option>Month</option>
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                      </select>
+                      <span><FaAngleDown className='icon' /></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
